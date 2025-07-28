9a3b6a79a761b46253128f6bf67840b86c572bc7
Rahul <136152536+rvahul@users.noreply.github.com>
2025-07-28 14:14:08 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from rembg import remove
+import io
+import plotly.graph_objects as go
+import plotly.express as px
+from datetime import datetime
+import pandas as pd
